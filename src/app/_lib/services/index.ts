1bd3456c9afb047be14585877ff8e15ceee17d86
Ali Muksin <ali.muksin0510@gmail.com>
2025-01-24 04:13:47 +0700

    refactor: change api call url with env value

--- src/app/_lib/services/index.ts
+++ src/app/_lib/services/index.ts
@@ -1,19 +1,20 @@
+import { env } from "@/env";
 import type { carouselDataType, sildeDataType, footerDataType } from "@/app/_lib/types";
 
 export async function getCarouselData(): Promise<carouselDataType[]> {
-  const response = await fetch("http://localhost:3000/api/carousels");
+  const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/carousels`);
   const data: carouselDataType[] = (await response.json()) as carouselDataType[];
   return data;
 }
 
 export async function getSlideData(): Promise<sildeDataType[]> {
-  const response = await fetch("http://localhost:3000/api/slides");
+  const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/slides`);
   const data: sildeDataType[] = (await response.json()) as sildeDataType[];
   return data;
 }
 
 export async function getFooterData(): Promise<footerDataType[]> {
-  const response = await fetch("http://localhost:3000/api/footers");
+  const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/footers`);
   const data: footerDataType[] = (await response.json()) as footerDataType[];
   return data;
 }
