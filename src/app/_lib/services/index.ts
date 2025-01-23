c846a22e5360b7bf3d948bfaddb8a96e05142b71
Ali Muksin <ali.muksin0510@gmail.com>
2025-01-24 04:20:24 +0700

    refactor: add error handling

--- src/app/_lib/services/index.ts
+++ src/app/_lib/services/index.ts
@@ -2,19 +2,34 @@ import { env } from "@/env";
 import type { carouselDataType, sildeDataType, footerDataType } from "@/app/_lib/types";
 
 export async function getCarouselData(): Promise<carouselDataType[]> {
-  const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/carousels`);
-  const data: carouselDataType[] = (await response.json()) as carouselDataType[];
-  return data;
+  try {
+    const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/carousels`);
+    const data: carouselDataType[] = (await response.json()) as carouselDataType[];
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 export async function getSlideData(): Promise<sildeDataType[]> {
-  const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/slides`);
-  const data: sildeDataType[] = (await response.json()) as sildeDataType[];
-  return data;
+  try {
+    const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/slides`);
+    const data: sildeDataType[] = (await response.json()) as sildeDataType[];
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 export async function getFooterData(): Promise<footerDataType[]> {
-  const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/footers`);
-  const data: footerDataType[] = (await response.json()) as footerDataType[];
-  return data;
+  try {
+    const response = await fetch(`${env.NEXT_PUBLIC_CLIENT_API_URL}/footers`);
+    const data: footerDataType[] = (await response.json()) as footerDataType[];
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
