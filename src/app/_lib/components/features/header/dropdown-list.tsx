3bade5de3b8d46ec99d77184ff679be5e5888119
Ali Muksin <ali.muksin0510@gmail.com>
2025-01-24 00:57:35 +0700

    refactor: add event on dropdown list menu

--- src/app/_lib/components/features/header/dropdown-list.tsx
+++ src/app/_lib/components/features/header/dropdown-list.tsx
@@ -3,12 +3,15 @@ import { twMerge } from "tailwind-merge";
 export default function List({
   children,
   className,
+  onClick,
 }: {
   children: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 }): React.ReactElement {
   return (
     <li
+      onClick={onClick}
       className={twMerge(
         "cursor-pointer px-4 py-2 hover:font-black hover:text-gray-900",
         className
