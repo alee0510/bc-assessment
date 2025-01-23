a6ec5c969de9d45f9f9bc319aadf9c357de43f6e
Ali Muksin <ali.muksin0510@gmail.com>
2025-01-23 20:19:00 +0700

    chore: implement hero and footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
+import HeroBanner from "@/app/_lib/components/features/hero-banner";
+import Footer from "@/app/_lib/components/features/footer";
+
 export default function HomePage(): React.ReactElement {
   return (
-    <main className="size-full">
-      <section className="relative isolate size-full min-h-[929px] bg-slate-300">
-        <div
-          id="backdrop"
-          className="fixed left-0 top-0 z-0 h-[929px] w-full bg-[url(/images/backdrop.png)] bg-cover bg-no-repeat opacity-80 mix-blend-color-burn"
-        />
-        <div
-          id="gradiant"
-          className="fixed left-0 top-0 h-[929px] w-full bg-gradient-to-t from-teal-400 to-blue-500 mix-blend-color-burn"
-        />
-      </section>
+    <main className="size-full overflow-hidden">
+      <HeroBanner />
+      <Footer />
     </main>
   );
 }
